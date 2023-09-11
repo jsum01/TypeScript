aef37782f44e5cacb10132aa19d4e3283f6793eb
jsum01 <djadjawltjr@gmail.com>
2023-09-11 15:47:09 +0900

    [Update] 커피 키오스크 count 개별동작 API 구현

--- TypeScript_Study/230910/src/App.tsx
+++ TypeScript_Study/230910/src/App.tsx
@@ -2,43 +2,64 @@ import { useState } from 'react';
 import './App.css';
 
 interface CoffeeMenu {
-  item: string[];
-  subitem: string[];
-  count: number;
+  menu: {
+    name: string,
+    option: [
+      {
+        optionName: string;
+        count: number;
+      },
+      {
+        optionName: string;
+        count: number;
+      },
+    ]
+  }
 }
-
 
 function App() {
 
-  const list: CoffeeMenu = {
-    item: ['아메리카노', '에스프레소', '카페라떼',],
-    subitem: [`샷추가`, `원두 변경`,],
-    count: 0,
+  // 커피메뉴 리스트
+  const list: CoffeeMenu[] = [ //객체배열을 사용한
+    { menu: { name: '아메리카노', option: [{ optionName: '디카페인', count: 0 }, { optionName: '샷추가', count: 0 },] } },
+    { menu: { name: '에스프레소', option: [{ optionName: '디카페인', count: 0 }, { optionName: '샷추가', count: 0 },] } },
+    { menu: { name: '카페라떼', option: [{ optionName: '디카페인', count: 0 }, { optionName: '샷추가', count: 0 }] } },
+  ]
+  // 카운트할 State
+  const [countState, setCountState] = useState<CoffeeMenu[]>(list); //State 타입지정 & 초기화
+  // Fucntion to Up | Down handle 
+  function handleUpDown(nameIndex: number, optionIndex: number, action: 'up' | 'down') {
+    const updateList = [...countState];
+    if (action === 'up') {
+      updateList[nameIndex].menu.option[optionIndex].count++;
+      setCountState(updateList);
+    } else if (action === 'down') {
+      updateList[nameIndex].menu.option[optionIndex].count--;
+      setCountState(updateList);
+    }
   }
-
-  const [countState, setCountState] = useState<CoffeeMenu>(list);
-
   return (
     <div className="App">
       <h1>Coffee Kiosk</h1>
       <ul>
-        {list.item.map((item, index) => {
+        {list.map((item, index) => {
           return <li key={index}>
-            {item}
+            {item.menu.name}
+            {/* name별 옵션 */}
             <ul>
-              {list.subitem.map((item, index) => {
-                return <li key={index}>
-                  {item}
-                  <button onClick={() => {setCountState(countState-1)}}>-</button>
-                  <span>{list.count}</span>
-                  <button onClick={() => {setCountState(countState+1)}}>+</button>
+              {item.menu.option.map((optionItem, optionIndex) => {
+                return <li key={optionIndex}>
+                  {optionItem.optionName}
+                  <button onClick={() => { handleUpDown(index, optionIndex, 'down') }}> - </button>
+                  <span> {countState[index].menu.option[optionIndex].count} </span>
+                  <button onClick={() => { handleUpDown(index, optionIndex, 'up') }}> + </button>
                 </li>
               })}
             </ul>
           </li>
         })}
-      </ul>
-    </div>
+      </ul >
+    </div >
   );
 }
 
